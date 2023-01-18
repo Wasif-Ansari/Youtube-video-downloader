7ff03e1731d7f654298eeea6d260e17f239daed6
Wasif Ansari <wasifansari2709@gmail.com>
2023-01-18 14:17:27 +0530

    making window using tkinter

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ from tkinter import *
 from tkinter import ttk
 from PIL import ImageTk,Image
 
+
+
+win = Tk()
+win.title("Youtube downloader")
+win.config(bg="green")
+win.geometry("800x600") # width x height
+
+win.mainloop()
