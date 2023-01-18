92b1f734df2f9b6efbc13456d1c40a1eff216dd1
Wasif Ansari <wasifansari2709@gmail.com>
2023-01-18 14:22:33 +0530

    making entry for link

--- main.py
+++ main.py
@@ -14,4 +14,7 @@ my_img = ImageTk.PhotoImage(Image.open("./download.png"))
 name = Label(win,text=" YOUTUBE VIDEO DOWNLOADER ",font=("Time New Roman",30,"bold"))
 name.place(x=25, y=50,height=50,width=750)
 
+entry = Entry(win,font=("Times New Roman", 15, "bold"))
+entry.place(x=25,y=140,height=50,width=600)
+
 win.mainloop()
