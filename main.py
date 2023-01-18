59793b7fb753d088ea04e3d29452d808da03382c
Wasif Ansari <wasifansari2709@gmail.com>
2023-01-18 14:34:25 +0530

    data get function for go button

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ from tkinter import ttk
 from PIL import ImageTk,Image
 
 
+def data_get():
+    link = entry.get()
+    youtube_1 = YouTube(link)
+    #print(youtube_1.title)
+    #print(youtube_1.thumbnail_url)
+
+    videos = youtube_1.streams.all() #get all stream options
+    videos = youtube_1.streams.filter(progressive=True) #for only audioṇ
+    vid = list(enumerate(videos)) #indexing
+    ans=""
+    for i in vid:
+        b = str(i).split("res")
+        mylist.insert(END,b[1][1:8])
+        # mylist.insert(END,str(i))
+
 
 win = Tk()
 win.title("Youtube downloader")
@@ -17,7 +32,7 @@ name.place(x=25, y=50,height=50,width=750)
 entry = Entry(win,font=("Times New Roman", 15, "bold"))
 entry.place(x=25,y=140,height=50,width=600)
 
-go_button = Button(win,text="GO>>",font=("Time New Roman",30,"bold"))
+go_button = Button(win,text="GO>>",font=("Time New Roman",30,"bold"),command=data_get)
 go_button.place(x=650,y=140,height=50,width=110)
 
 mylist = Listbox(win)
