cb8f5ced2d28217f220332d86104faef1f7e9fbd
Wasif Ansari <wasifansari2709@gmail.com>
2023-01-18 14:21:40 +0530

    download image

--- main.py
+++ main.py
@@ -10,4 +10,8 @@ win.title("Youtube downloader")
 win.config(bg="green")
 win.geometry("800x600") # width x height
 
+my_img = ImageTk.PhotoImage(Image.open("./download.png"))
+name = Label(win,text=" YOUTUBE VIDEO DOWNLOADER ",font=("Time New Roman",30,"bold"))
+name.place(x=25, y=50,height=50,width=750)
+
 win.mainloop()
